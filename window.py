a96dc0ec3202d69ec270fda4c247a50e1ea2da49
Darkkz <jake.diedrich@gmail.com>
2023-09-02 15:27:00 -0400

    Finish UI Additons

--- window.py
+++ window.py
@@ -1,9 +1,12 @@
 import tkinter as tk
 import tkinter.ttk as ui
 from tkinter import filedialog
+import matplotlib as plot
 
 class Window:
     graph = ''
+    label = ''
+    filename = ''
 
     def __init__(self, width, height):
         self.width = width
@@ -15,23 +18,29 @@ class Window:
         window.geometry(self.width + 'x' + self.height)
         window.title('Data Reader')
 
+        # Left Button Frame (Swap out to have a menu frame and a graph frame)
+        menuFrame = tk.LabelFrame(window, text='Menu Options', padx=10, pady=10, width=170, height=300, labelanchor='n')
+        menuFrame.pack(expand="yes", padx=20, pady=20, side='left')
+        menuFrame.propagate(False)
+
         # New Window Button
-        button = ui.Button(window, text = 'New Window', command = self.renderWindow)
-        button.place(x = 10, y = 10)
+        button = ui.Button(menuFrame, text='New Window', command = self.renderWindow)
+        button.place(x=0, y=0)
         button.config(width = 20)
 
         # Settings Button
-        button = ui.Button(window, text = 'Settings', command = self.openSettings)
-        button.place(x = 10, y = 50)
+        button = ui.Button(menuFrame, text='Settings', command=self.openSettings)
+        button.place(x=0, y=40)
         button.config(width = 20)
 
         # Upload Data Button
-        button = ui.Button(window, text = 'Upload Data', command = self.parseData)
-        button.place(x = 10, y = 90)
-        button.config(width = 20)
+        button = ui.Button(menuFrame, text='Upload Data', command=self.parseData)
+        button.place(x=0, y=80)
+        button.config(width=20)
 
         # Dropdown Menu
         dropdown = ui.Combobox(
+            menuFrame,
             state = 'readonly',
             values = [
                 'Bar',
@@ -39,12 +48,30 @@ class Window:
                 'Line'
             ]
         )
-        dropdown.place(x = 10, y = 130)
+        dropdown.place(x=0, y=120)
+        dropdown.current(0)
+        self.graph = 'Bar'
 
         # Start Render Button
-        button = ui.Button(window, text = 'Start Render', command = self.startRender)
-        button.place(x = 10, y = 170)
-        button.config(width = 20)
+        button = ui.Button(menuFrame, text='Start Render', command=self.startRender)
+        button.place(x=0, y=160)
+        button.config(width=20)
+
+        # Graph Frame
+        graphFrame = tk.LabelFrame(window, text='Data Enhancement', padx=10, pady=10, width=1100, height=500, labelanchor='n')
+        graphFrame.pack(expand="yes", padx=20, pady=20)
+        graphFrame.propagate(False)
+
+        # Add Graph in graphFrame
+
+        # Log Frame
+        logFrame = tk.LabelFrame(window, text='Logs', padx=10, pady=10, width=1500, height=100, labelanchor='n')
+        logFrame.pack(expand="yes", padx=20, pady=20)
+        logFrame.propagate(False)
+
+        # Log Label
+        self.label = tk.Label(logFrame, text='Welcome!')
+        self.label.pack(expand="yes", padx=20, pady=20)
 
         # Mainloop call at the end of the rendering
         tk.mainloop()
@@ -61,11 +88,22 @@ class Window:
             ('Excel files', '*.xlsx'),
             ('CSV Files', '*.csv')
         )
-        filename = filedialog.askopenfilename(filetypes = filetypes)
-        print(filename)
+        self.filename = filedialog.askopenfilename(filetypes=filetypes)
+        self.updateLogMessage('Successfully Uploaded File: ' + self.filename)
 
     def startRender(self):
+        if self.filename == '':
+            self.updateLogMessage('ERROR: There is No File Attached')
+            return
+        
+        self.updateLogMessage('Rendering a ' + self.graph + ' graph from file: ' + self.filename)
         print('rendering data')
 
     def updateWindow(width, height):
         print('updating width and height of window')
+
+    def updateLogMessage(self, message):
+        self.label.config(text=message)
+
+    def setGraph(self, graph):
+        self.graph = graph
